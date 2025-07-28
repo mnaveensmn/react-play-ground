39d4c44bd2457596d7d68bcca42469f1d5445ace
Naveen Kumar <naveen.kumar2@rxglobal.com>
2025-07-28 08:16:32 +0530

    native-support-exploration: Refactor the main page

--- native-support-exploration/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders native support exploration text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Native Support Exploration/i);
-  expect(linkElement).toBeInTheDocument();
-});
